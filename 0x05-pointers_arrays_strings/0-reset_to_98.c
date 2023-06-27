0866da5867a79a9a89727c3ee75506996cf3ede3
akrammalyadi <akram.malyadi@emines.um6p.ma>
2023-06-27 15:58:34 -0700

    c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,23 +2,25 @@
 
 /**
  * reset_to_98 - Updates the value pointed to by n to 98.
- * @n: Pointer to an integer.
  *
  * Description: This function updates the value of the integer pointed to by n
  *              to 98.
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+    *n = 98;
 }
+
+
+void reset_to_98(int *n);  /* Function prototype */
 
 int main(void)
 {
-	int num = 10;
+    int num = 10;
 
-	printf("Before reset: %d\n", num);
-	reset_to_98(&num);
-	printf("After reset: %d\n", num);
+    printf("Before reset: %d\n", num);
+    reset_to_98(&num);
+    printf("After reset: %d\n", num);
 
-	return 0;
+    return 0;
 }
