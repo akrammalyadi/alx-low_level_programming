2d050eeaf1aaa497ea33be2a12dda6c2a8dbb7d1
akrammalyadi <akram.malyadi@emines.um6p.ma>
2023-06-27 17:12:53 -0700

    c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	/* Handling sign */
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[0] == '+')
+	{
+		i++;
+	}
+
+	/* Converting string to integer */
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			/* Stop conversion if a non-digit character is encountered */
+			break;
+		}
+
+		i++;
+	}
+
+	return result * sign;
+}
